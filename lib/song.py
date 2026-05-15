f316ddc7b94ec1246f8b46fabf3ec65846c4e66a
farhiya omar <queenfonsa@gmail.com>
2026-05-15 09:58:32 +0300

    done

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,74 @@
 class Song:
-    pass
+    # Class attributes
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        # Instance attributes
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Update class data whenever a new song is created
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    # Class method to increase song count
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    # Add unique genres
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    # Add unique artists
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    # Count songs by genre
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    # Count songs by artist
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+
+# Example usage
+song1 = Song("99 Problems", "Jay-Z", "Rap")
+song2 = Song("Halo", "Beyonce", "Pop")
+song3 = Song("Empire State of Mind", "Jay-Z", "Rap")
+
+print(Song.count)
+# 3
+
+print(Song.genres)
+# ['Rap', 'Pop']
+
+print(Song.artists)
+# ['Jay-Z', 'Beyonce']
+
+print(Song.genre_count)
+# {'Rap': 2, 'Pop': 1}
+
+print(Song.artist_count)
+# {'Jay-Z': 2, 'Beyonce': 1}
